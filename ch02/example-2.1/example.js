e89452517882f7b87aa8a9fb8188d866b0d1348e
plding <77676182@qq.com>
2018-02-11 21:37:18 +0800

    update

---
+++ ch02/example-2.1/example.js
@@ -0,0 +1,14 @@
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+
+context.lineWidth = 30;
+
+context.font = '24px Helvetica';
+context.fillText('Click anywhere to erase', 175, 40);
+
+context.strokeRect(75, 100, 200, 200);
+context.fillRect(325, 100, 200, 200);
+
+canvas.onmousedown = function (e) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+};
