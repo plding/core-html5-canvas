ce419e8878698fe5f82d4b03426331c6665bfe79
plding <77676182@qq.com>
2018-07-13 15:32:54 +0800

    update

--- ch02/example-2.1/example.js
+++ ch02/example-2.1/example.js
@@ -9,6 +9,6 @@ context.fillText('Click anywhere to erase', 175, 40);
 context.strokeRect(75, 100, 200, 200);
 context.fillRect(325, 100, 200, 200);
 
-canvas.onmousedown = function (e) {
+context.canvas.onmousedown = function(e) {
     context.clearRect(0, 0, canvas.width, canvas.height);
 };
--- ch02/example-2.5/example.js
+++ ch02/example-2.5/example.js
@@ -6,36 +6,31 @@ var canvas = document.getElementById('canvas'),
     repeatYRadio = document.getElementById('repeatYRadio'),
     image = new Image();
 
-// Functions.....................................................
-
 function fillCanvasWithPattern(repeatString) {
     var pattern = context.createPattern(image, repeatString);
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = pattern;
     context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fill();
 }
 
-// Event handlers................................................
-
-repeatRadio.onclick = function (e) {
+repeatRadio.onclick = function(e) {
     fillCanvasWithPattern('repeat');
 };
 
-repeatXRadio.onclick = function (e) {
+repeatXRadio.onclick = function(e) {
     fillCanvasWithPattern('repeat-x');
 };
 
-repeatYRadio.onclick = function (e) {
+repeatYRadio.onclick = function(e) {
     fillCanvasWithPattern('repeat-y');
 };
 
-noRepeatRadio.onclick = function (e) {
+noRepeatRadio.onclick = function(e) {
     fillCanvasWithPattern('no-repeat');
 };
 
-// Initialization................................................
-
 image.src = 'redball.png';
-image.onload = function (e) {
+image.onload = function(e) {
     fillCanvasWithPattern('repeat');
 };
